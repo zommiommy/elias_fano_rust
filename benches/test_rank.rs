74d433131417f3a3c440c698ebf627e102b9ca0b
zom <tommaso.fontana.96@gmail.com>
2020-09-29 22:08:30 +0200

    added simd feature

--- benches/test_rank.rs
+++ benches/test_rank.rs
@@ -1,11 +1,9 @@
 #![feature(test)]
 #![allow(clippy::unreadable_literal)]
 
-extern crate fid;
 extern crate rand;
 extern crate test;
 
-use fid::{BitVector, FID};
 use rand::{Rng, SeedableRng};
 use rand::{RngCore};
 use rand::rngs::SmallRng;
@@ -20,20 +18,6 @@ const SEED: [u8; 16] = [
     0xc0, 0xfe, 0xbe, 0xbe   
 ];
 
-#[bench]
-fn rank_1_000_000_dense(b: &mut Bencher) {
-    bench_rank1(1_000_000, 0.99, b);
-}
-
-#[bench]
-fn rank_1_000_000_normal(b: &mut Bencher) {
-    bench_rank1(1_000_000, 0.5, b);
-}
-
-#[bench]
-fn rank_1_000_000_sparse(b: &mut Bencher) {
-    bench_rank1(1_000_000, 0.01, b);
-}
 
 #[bench]
 fn rank_1_000_000_vec(b: &mut Bencher) {
@@ -46,21 +30,6 @@ fn rank_1_000_000_ef(b: &mut Bencher) {
 }
 
 #[bench]
-fn rank_100_000_000_dense(b: &mut Bencher) {
-    bench_rank1(100_000_000, 0.99, b);
-}
-
-#[bench]
-fn rank_100_000_000_normal(b: &mut Bencher) {
-    bench_rank1(100_000_000, 0.5, b);
-}
-
-#[bench]
-fn rank_100_000_000_sparse(b: &mut Bencher) {
-    bench_rank1(100_000_000, 0.01, b);
-}
-
-#[bench]
 fn rank_100_000_000_vec(b: &mut Bencher) {
     bench_vector_rank(100_000_000, b);
 }
@@ -68,20 +37,6 @@ fn rank_100_000_000_vec(b: &mut Bencher) {
 #[bench]
 fn rank_100_000_000_ef(b: &mut Bencher) {
     bench_ef_rank(100_000_000, b);
-}
-
-fn bench_rank1(n: u64, p: f64, b: &mut Bencher) {
-    let mut rng: SmallRng = SmallRng::from_seed(SEED);
-    let mut bv = BitVector::new();
-    for _ in 0..n {
-        let b = rng.gen_bool(p);
-        bv.push(b);
-    }
-    b.iter(|| {
-        for _ in 0..TRIALS {
-            black_box(bv.rank1(rng.gen_range(0, n)));
-        }
-    })
 }
 
 fn bench_vector_rank(n: u64, b: &mut Bencher) {
--- benches/test_select.rs
+++ benches/test_select.rs
@@ -1,17 +1,15 @@
 #![feature(test)]
 #![allow(clippy::unreadable_literal)]
 
-extern crate fid;
 extern crate rand;
 extern crate test;
 
-use fid::{BitVector, FID};
 use rand::{Rng, SeedableRng};
 use rand::{RngCore};
 use rand::rngs::SmallRng;
 use test::{Bencher, black_box};
 
-const TRIALS: u64 = 10_000;
+const TRIALS: u64 = 1_000;
 
 const SEED: [u8; 16] = [
     0xde, 0xad, 0xbe, 0xef,
@@ -19,21 +17,6 @@ const SEED: [u8; 16] = [
     0xde, 0xad, 0xbe, 0xef,
     0xc0, 0xfe, 0xbe, 0xbe   
 ];
-
-#[bench]
-fn select_1_000_000_dense(b: &mut Bencher) {
-    bench_select1(1_000_000, 0.99, b);
-}
-
-#[bench]
-fn select_1_000_000_normal(b: &mut Bencher) {
-    bench_select1(1_000_000, 0.5, b);
-}
-
-#[bench]
-fn select_1_000_000_sparse(b: &mut Bencher) {
-    bench_select1(1_000_000, 0.01, b);
-}
 
 #[bench]
 fn select_1_000_000_vec(b: &mut Bencher) {
@@ -46,21 +29,6 @@ fn select_1_000_000_ef(b: &mut Bencher) {
 }
 
 #[bench]
-fn select_100_000_000_dense(b: &mut Bencher) {
-    bench_select1(100_000_000, 0.99, b);
-}
-
-#[bench]
-fn select_100_000_000_normal(b: &mut Bencher) {
-    bench_select1(100_000_000, 0.5, b);
-}
-
-#[bench]
-fn select_100_000_000_sparse(b: &mut Bencher) {
-    bench_select1(100_000_000, 0.01, b);
-}
-
-#[bench]
 fn select_100_000_000_vec(b: &mut Bencher) {
     bench_vector_select(100_000_000, b);
 }
@@ -70,22 +38,6 @@ fn select_100_000_000_ef(b: &mut Bencher) {
     bench_ef_select(100_000_000, b);
 }
 
-
-fn bench_select1(n: u64, p: f64, b: &mut Bencher) {
-    let mut rng: SmallRng = SmallRng::from_seed(SEED);
-    let mut bv = BitVector::new();
-    let mut rank = 0;
-    for _ in 0..n {
-        let b = rng.gen_bool(p);
-        bv.push(b);
-        rank += b as u64;
-    }
-    b.iter(|| {
-        for _ in 0..TRIALS {
-            black_box(bv.select1(rng.gen_range(0, rank)));
-        }
-    })
-}
 
 
 fn bench_vector_select(n: u64, b: &mut Bencher) {

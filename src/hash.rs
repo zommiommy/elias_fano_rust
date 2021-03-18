7bdf110b7710cd9fe98a2117dbf675d6436a1a56
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2021-03-18 19:22:25 +0100

    simplified the hash

--- src/hash.rs
+++ src/hash.rs
@@ -3,17 +3,6 @@ use std::hash::{Hash, Hasher};
 
 impl Hash for EliasFano {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        self.universe.hash(state);
-        self.number_of_elements.hash(state);
-        self.low_bits.hash(state);
-        self.high_bits.hash(state);
-        
-        // These are helper methods and should not matter
-        // self.last_high_value.hash(state);
-        // self.last_value.hash(state);
-        // self.last_index.hash(state);
-        // self.current_number_of_elements.hash(state);
-        // self.low_bit_count.hash(state);
-        // self.low_bit_mask.hash(state);
+        self.iter().for_each(|x| x.hash(state));
     }
 }

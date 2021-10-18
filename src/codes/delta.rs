93e765dcb16da1f4232d766fa373e5ed5415c944
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2021-10-19 00:30:59 +0200

    delta works

--- src/codes/delta.rs
+++ src/codes/delta.rs
@@ -5,17 +5,16 @@ use super::BitStream;
 // preceded by a representation of its length in γ.
 impl BitStream {
 
-    // TODO FIX THIS SHIT
     #[inline]
     pub fn read_delta(&mut self) -> u64 {
         let len = self.read_gamma();
-        self.read_bits(len) - 1
+        self.read_bits(len)
     }
 
     #[inline]
-    pub fn write_delta(&mut self, mut value: u64) {
-        value += 1;
-        let number_of_blocks_to_write = fast_log2_ceil(value);
+    pub fn write_delta(&mut self, value: u64) {
+        // TODO!: figure out if the +1 is actually needed
+        let number_of_blocks_to_write = fast_log2_ceil(value + 1);
         self.write_gamma(number_of_blocks_to_write);
         self.write_bits(number_of_blocks_to_write, value);
     }
@@ -25,7 +24,7 @@ impl BitStream {
 mod test_delta {
     use super::*;
 
-    //#[test]
+    #[test]
     /// Test that we encode and decode low bits properly.
     fn test_delta_forward() {
         let mut bs = BitStream::new();
@@ -39,7 +38,7 @@ mod test_delta {
         }
     }
 
-    //#[test]
+    #[test]
     /// Test that we encode and decode low bits properly.
     fn test_delta_backward() {
         let mut bs = BitStream::new();
